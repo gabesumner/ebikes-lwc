180cc570bd2a391bc68eeca297cd339176f6beb0
Gabe Sumner <gsumner@gsumner-ltm7.internal.salesforce.com>
2019-05-10 16:22:36 -0700

    Changes to orgInit

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,8 @@
-sfdx force:org:create -s -f config/project-scratch-def.json -a ebikes
+sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s -n --userprefix ebikes --userdomain ebikes.demo
 sfdx force:source:push
 sfdx force:user:permset:assign -n ebikes
 sfdx force:data:tree:import --plan ./data/sample-data-plan.json
+sfdx shane:org:reauth -r --json
+sfdx shane:theme:activate -n eBikesTheme
 sfdx force:org:open
 sfdx shane:user:password:set -g User -l User -p sfdx1234
